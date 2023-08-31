52376caa85b87ddcbe7ff55bd83df44291023a93
Humayun121 <Humayun121@users.noreply.github.com>
2023-08-31 21:29:59 +0100

    Signup forms,views and url

---
+++ socials/accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path,include
+from django.contrib.auth import views as auth_views
+from . import views
+
+app_name = 'accounts'  #template tagging
+
+urlpattenrs = [
+    path('signup/',views.SignUp.as_view(),name='signup'),
+]
--- socials/accounts/views.py
+++ socials/accounts/views.py
@@ -5,4 +5,7 @@ from django.views.generic import CreateView
 
 from . import forms
 
-
+class SignUp(CreateView):
+    formClass = forms.UserCreateForm
+    success_url = reverse_lazy("login")
+    template_name = "accounts/signup.html"
