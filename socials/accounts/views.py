7c99f78efeb3c1a390e65a3dfba8c94a04d68a15
Humayun121 <Humayun121@users.noreply.github.com>
2023-09-01 18:11:30 +0100

    Added the Signup/Login features

--- socials/accounts/views.py
+++ socials/accounts/views.py
@@ -6,6 +6,6 @@ from django.views.generic import CreateView
 from . import forms
 
 class SignUp(CreateView):
-    formClass = forms.UserCreateForm
+    form_class = forms.UserCreateForm
     success_url = reverse_lazy("login")
     template_name = "accounts/signup.html"
