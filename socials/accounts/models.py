c8b1372e00d64b127cfe67aa433a41f1d7d70544
Humayun121 <Humayun121@users.noreply.github.com>
2023-09-10 19:23:49 +0100

    Updated the User class

--- socials/accounts/models.py
+++ socials/accounts/models.py
@@ -7,33 +7,13 @@ UserModel = get_user_model()
 
 # Create your models here.
 class User(auth.models.User, auth.models.PermissionsMixin):
+    user = models.OneToOneField(UserModel, on_delete=models.CASCADE)
+    followers = models.ManyToManyField(
+        UserModel,
+        related_name="following",
+        blank=True
+    )
+    
+
     def __str__(self):
         return f"@{self.username}"
-
-
-# Following
-class UserFollowing(models.Model):
-    user_id = models.ForeignKey(
-        UserModel, related_name="following", on_delete=models.CASCADE
-    )
-    following_user_id = models.ForeignKey(
-        UserModel,
-        related_name="follwoing",
-        on_delete=models.CASCADE,
-    )
-    created = models.DateTimeField(
-        auto_now_add=True,
-        db_index=True
-    )
-
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(
-                fields=["user_id", "following_user_id"], name="unique_followers"
-            )
-        ]
-
-        ordering = ["-created"]
-
-    def __str__(self):
-        f"{self.user_id} follows {self.following_user_id}"
